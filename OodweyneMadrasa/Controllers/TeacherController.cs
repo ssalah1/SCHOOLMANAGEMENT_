9cf6f12664befa65acf5c85c5030451c35a9f298
ssalah1 <saidsalah014@gmail.com>
2020-03-20 13:19:26 -0400

    added teacher controller and its view

---
+++ OodweyneMadrasa/Controllers/TeacherController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using OodweyneMadrasa.Models;
+
+namespace OodweyneMadrasa.Controllers
+{
+    public class TeacherController : Controller
+    {
+        private readonly SchoolDatabase _db;
+        public TeacherController(SchoolDatabase db)
+        {
+            _db = db;  
+        }
+
+        public IActionResult AllTeachers()
+        {
+            var showAllTeachers = _db.Teachers.Include(s => s.Students).ToList();
+            return View(showAllTeachers);
+        }
+        public IActionResult CreateTeacher()
+        {
+            return View();
+        }
+       
+    }
+}
